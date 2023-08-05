8b4354e58d0fafeb2317eb9bdffdf75037ff5fc4
Daniil Zahliadov <30547089+zagliadov@users.noreply.github.com>
2023-08-05 08:53:48 +0300

    Documentation and typing useUserStore

--- client/src/store/modules/user.ts
+++ client/src/store/modules/user.ts
@@ -10,6 +10,12 @@ export const useUserStore = defineStore("user", () => {
   const authStore = useAuthStore();
   const { existingUser } = storeToRefs(authStore);
 
+  /**
+   * Uploads a photo and updates the existing user's data.
+   *
+   * @param file - The file to upload.
+   * @returns The updated existing user's data, or undefined if there's an issue.
+   */
   const uploadPhoto = async (
     file: File
   ): Promise<IExistingUser | undefined> => {
@@ -18,7 +24,7 @@ export const useUserStore = defineStore("user", () => {
     try {
       const formData = new FormData();
       formData.append("File", file);
-      const response = await axios.post(
+      const response = await axios.post<{ existingUser: IExistingUser }>(
         `${API_URL}/user/upload_photo`,
         formData,
         {
@@ -28,7 +34,7 @@ export const useUserStore = defineStore("user", () => {
         }
       );
       if (response.status === 200) {
-        return existingUser.value = response.data.existingUser;
+        return (existingUser.value = response.data.existingUser);
       } else {
         console.log("Failed to upload photo");
       }
@@ -37,11 +43,16 @@ export const useUserStore = defineStore("user", () => {
     }
   };
 
+  /**
+   * Removes the avatar of the existing user and updates their data.
+   *
+   * @returns The updated existing user's data, or undefined if there's an issue.
+   */
   const removeAvatar = async (): Promise<IExistingUser | undefined> => {
     const token = localStorage.getItem("token");
     if (!token) return;
     try {
-      const response = await axios.post(
+      const response = await axios.post<{ existingUser: IExistingUser }>(
         `${API_URL}/user/remove_avatar_filename`,
         {},
         {
@@ -52,7 +63,7 @@ export const useUserStore = defineStore("user", () => {
       );
       if (response.status === 200) {
         if (existingUser.value) {
-          return existingUser.value = response.data.existingUser;
+          return (existingUser.value = response.data.existingUser);
         }
       } else {
         console.log("Failed to remove avatar");
