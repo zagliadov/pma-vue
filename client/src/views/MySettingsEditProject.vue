4d15996024be86047cad2e85b68f8c17287e4c15
Daniil <portal.sid.grand2@gmail.com>
2023-08-18 15:11:58 +0300

    fix

--- client/src/views/MySettingsEditProject.vue
+++ client/src/views/MySettingsEditProject.vue
@@ -45,7 +45,14 @@ const hoveredMember = ref<string | null>(null);
         </div>
       </div>
       <div class="pt-10">
-        <span class="font-medium"> Members ({{ membersCount }}) </span>
+        <div class="flex items-center justify-between">
+          <span class="font-medium"> Members ({{ membersCount }}) </span>
+          <button class="btn btn-primary">
+            <IconPlus class="stroke-base-100" />
+            <span>add members</span>
+          </button>
+        </div>
+
         <div
           v-for="{ email, avatar_filename, firstName, lastName } in members"
           class="flex items-center justify-between rounded p-2 mt-4 hover:bg-base-200"
@@ -74,6 +81,19 @@ const hoveredMember = ref<string | null>(null);
           </div>
 
           <button v-if="hoveredMember === email"><IconClose /></button>
+        </div>
+
+        <div class="flex flex-col pt-10">
+          <span class="font-medium">Manage project</span>
+          <span class="text-sm text-gray-600 pt-5"
+            >Deleting a project is an irreversible action. After deleting the
+            project, all data will be lost</span
+          >
+          <div class="pt-5">
+            <button class="btn btn-error text-error-content text-opacity-50">
+              Delete project
+            </button>
+          </div>
         </div>
       </div>
     </div>
