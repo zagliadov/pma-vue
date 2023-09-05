9d23bd5132c4382e9ab54a6a3625641e0213fb7f
Daniil <portal.sid.grand2@gmail.com>
2023-09-05 20:07:03 +0300

    Refactored createTask function for improved readability and modularity.

--- server/src/controllers/task.ts
+++ server/src/controllers/task.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { Request, Response } from "express";
-import { handleError } from "../helpers/helpers";
 import prisma from "../db";
 import * as _ from "lodash";
 import {
@@ -12,12 +11,9 @@ import {
   boolean,
   parse,
   nullable,
-  union,
-  nullType,
   optional,
 } from "valibot";
 import { ITaskAssignee } from "./interfaces";
-import fileUpload, { UploadedFile } from "express-fileupload";
 
 type TaskData = {
   taskName: string;
@@ -55,33 +51,110 @@ const TaskSchema = object({
   taskAssignee: TaskAssigneeSchema,
 });
 
+const fileNameGenerator = (projectId: number, file: any) => {
+  return `${projectId}-${file.name}`;
+};
+
+// Helper function to move files
+const moveFiles = async (
+  taskFileArray: any,
+  destinationPath: string,
+  projectId: number
+) => {
+  try {
+    await Promise.all(
+      _.map(taskFileArray, async (file) => {
+        const newFileName = encodeURI(fileNameGenerator(projectId, file));
+        await file.mv(`${destinationPath}${newFileName}`);
+      })
+    );
+  } catch (error) {
+    console.error("Error moving files:", error);
+    throw new Error("Failed to move files");
+  }
+};
+
+// Helper function to create task assignees
+type assignee = {
+  userId: number;
+  email: string;
+  taskId: number;
+  projectCreator: boolean;
+  isEmailConfirmed: boolean;
+};
+const createTaskAssignees = async (taskAssignees: assignee[]) => {
+  try {
+    await Promise.all(
+      _.map(taskAssignees, async (assignee) => {
+        await prisma.taskAssignee.create({
+          data: {
+            userId: assignee.userId,
+            email: assignee.email,
+            taskId: assignee.taskId,
+            projectCreator: assignee.projectCreator,
+            isEmailConfirmed: assignee.isEmailConfirmed,
+          },
+        });
+      })
+    );
+  } catch (error) {
+    console.error("Error creating task assignees:", error);
+    throw new Error("Failed to create task assignees");
+  }
+};
+
+const createNewTask = async (taskData: TaskData, projectId: number) => {
+  const newTask = await prisma.task.create({
+    data: {
+      name: taskData.taskName,
+      status: taskData.taskStatus,
+      description: taskData.taskDescription,
+      projectId: projectId,
+    },
+  });
+
+  return newTask;
+};
+
+// Helper function to create task files
+type newTask = {
+  id: number;
+  projectId: number;
+};
+const createTaskFiles = async (taskFileArray: any, newTask: newTask) => {
+  try {
+    await Promise.all(
+      _.map(taskFileArray, async (file) => {
+        await prisma.taskFile.create({
+          data: {
+            taskId: newTask.id,
+            fileName: fileNameGenerator(newTask.projectId, file),
+          },
+        });
+      })
+    );
+  } catch (error) {
+    console.error("Error creating task files:", error);
+    throw new Error("Failed to create task files");
+  }
+};
+
 export const createTask = async (req: any, res: Response) => {
   try {
     const { email, id } = req.userData;
     const { userData, projectId } = req.body;
     const taskFileArray: any = req?.files;
     const destinationPath = `${__dirname}/taskFiles/`;
-    await Promise.all(
-      _.map(taskFileArray, async (file) => {
-        const newFileName = encodeURI(projectId + "-" + file.name);
-        await file.mv(`${destinationPath}${newFileName}`);
-      })
-    );
+    await moveFiles(taskFileArray, destinationPath, projectId);
     const task: TaskData = parse(TaskSchema, JSON.parse(userData));
-    const newTask = await prisma.task.create({
-      data: {
-        name: task.taskName,
-        status: task.taskStatus,
-        description: task.taskDescription,
-        projectId: Number(projectId),
-      },
-    });
+    const newTask = await createNewTask(task, Number(projectId));
 
-    // Create an array of tasks to assign
-    // The code creates an array called taskAssignees using await Promise.all(). This array contains an initial object with data related to the current user (userId, email, taskId, projectCreator, and isEmailConfirmed).
-    // Additionally, it maps over the task.taskAssignee array, filtering out elements where the assignee.email is not equal to the current user's email. For each filtered element, it asynchronously queries the prisma.user database to find unique user data based on the assignee.email.
-    // The mapped result is an array of objects, each containing information about an assignee for the task. This information includes the user's userId, email, taskId, projectCreator, and isEmailConfirmed.
-    // In summary, taskAssignees is an array that combines the current user's information with information about other assignees for a task, fetched from the database. The resulting array is an important part of the task creation process.
+    // Create an array of task assignees by combining the current user's information and information about other assignees.
+    // The code starts by creating an array called taskAssignees, which includes an initial object with data related to the current user (userId, email, taskId, projectCreator, and isEmailConfirmed).
+    // Then, it maps over the task.taskAssignee array, filtering out elements where the assignee.email is not equal to the current user's email.
+    // For each filtered element, it asynchronously queries the prisma.user database to find unique user data based on the assignee.email.
+    // The mapped result is an array of objects, each containing information about an assignee for the task, including userId, email, taskId, projectCreator, and isEmailConfirmed.
+    // This array is a crucial part of the task creation process.
     const taskAssignees = await Promise.all([
       {
         userId: id,
@@ -109,32 +182,9 @@ export const createTask = async (req: any, res: Response) => {
     ]);
 
     // Creating Assigned User Records
-    await Promise.all(
-      _.map(taskAssignees, async (assignee) => {
-        console.log(assignee);
-        await prisma.taskAssignee.create({
-          data: {
-            userId: assignee.userId,
-            email: assignee.email,
-            taskId: assignee.taskId,
-            projectCreator: assignee.projectCreator,
-            isEmailConfirmed: assignee.isEmailConfirmed,
-          },
-        });
-      })
-    );
-
+    await createTaskAssignees(taskAssignees);
     // Creating task file records
-    await Promise.all(
-      _.map(taskFileArray, async (file) => {
-        await prisma.taskFile.create({
-          data: {
-            taskId: newTask.id,
-            fileName: projectId + "-" + file.name,
-          },
-        });
-      })
-    );
+    await createTaskFiles(taskFileArray, newTask);
 
     res.status(201).json({ message: "Task created successfully" });
   } catch (error) {
