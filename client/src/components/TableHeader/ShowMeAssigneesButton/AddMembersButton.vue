d5992c84c81eabe399c78701a9bf0404090ade5d
Daniil Zahliadov <30547089+zagliadov@users.noreply.github.com>
2023-10-04 20:17:39 +0300

    Initializing a modal window to add a new assignee

--- client/src/components/TableHeader/ShowMeAssigneesButton/AddMembersButton.vue
+++ client/src/components/TableHeader/ShowMeAssigneesButton/AddMembersButton.vue
@@ -1,16 +1,47 @@
 <script setup lang="ts">
+import { ref } from "vue";
+const { projectId } = defineProps(["projectId"]);
+const newAssigneeEmail = ref<string>("");
 
-const { projectId } = defineProps(["projectId"])
-const handleAddMembers = () => {
-  console.log("add members fn", projectId);
+const handleAddAssignee = (projectId: number, newAssigneeEmail: string) => {
+  console.log("add members fn", projectId, newAssigneeEmail);
+};
+
+const handleOpenModal = () => {
+  const modal: HTMLDialogElement | null =
+    document.querySelector("#add_new_assignee");
+  if (modal) modal?.showModal();
 };
 </script>
 
 <template>
   <div class="px-4 py-2">
-    <button class="btn w-full bg-green-50" @click="handleAddMembers">
+    <button class="btn w-full bg-green-50" @click="handleOpenModal">
       <IconPlus />
       <span class="text-primary">Add members</span>
     </button>
+    <dialog id="add_new_assignee" class="modal">
+      <div class="modal-box">
+        <h3 class="font-bold text-lg pb-4">Add a new assignee</h3>
+        <div class="flex relative form-control w-full">
+          <CustomInput
+            v-model="newAssigneeEmail"
+            placeholder="Type here"
+            name="Enter the email address of the new assignee"
+          />
+        </div>
+        <div class="modal-action">
+          <form method="dialog">
+            <button class="btn">Close</button>
+            <button
+              class="btn btn-primary ml-3"
+              @click="handleAddAssignee(projectId, newAssigneeEmail)"
+            >
+              Add a new assignee
+            </button>
+          </form>
+        </div>
+      </div>
+    </dialog>
   </div>
 </template>
