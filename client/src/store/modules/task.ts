b3a87a34ed734727f0aa2cc3eda7ee8139706a93
Daniil Zahliadov <30547089+zagliadov@users.noreply.github.com>
2023-10-07 20:41:53 +0300

    Typing, documentation, updated error handling for useTaskStore.

--- client/src/store/modules/task.ts
+++ client/src/store/modules/task.ts
@@ -1,9 +1,19 @@
 import { defineStore } from "pinia";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, type AxiosResponse } from "axios";
 import { API_URL } from "../../helpers/constants";
 import type { ICreateTask } from "../interfaces";
 
 export const useTaskStore = defineStore("task", () => {
+  
+  /**
+   * Creates a new task with the provided user data and attaches files to it for a specific project.
+   *
+   * @param {ICreateTask} userData - The user data for creating the task.
+   * @param {number} projectId - The ID of the project to which the task belongs.
+   * @param {File[]} taskFileArray - An array of files to be attached to the task.
+   * @throws {Error} If the user is not authenticated, the request fails, or a server error occurs.
+   * @returns {Promise<void>} A Promise that resolves once the task is successfully created.
+   */
   const createTask = async (
     userData: ICreateTask,
     projectId: number,
@@ -11,7 +21,7 @@ export const useTaskStore = defineStore("task", () => {
   ): Promise<void> => {
     try {
       const token: string | null = localStorage.getItem("token");
-      if (!token) return;
+      if (!token) throw new Error("User is not authenticated");
       const formData: FormData = new FormData();
       formData.append("userData", JSON.stringify(userData));
       formData.append("projectId", projectId.toString());
@@ -19,17 +29,24 @@ export const useTaskStore = defineStore("task", () => {
       taskFileArray.forEach((file: File, index: number) => {
         formData.append(`taskFileArray[${index}]`, file);
       });
-      await axios.post(`${API_URL}/task/create_task`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response: AxiosResponse<{ status: boolean }> = await axios.post(
+        `${API_URL}/task/create_task`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        console.log("The task is successfully created.");
+      }
     } catch (error: AxiosError | unknown) {
       if (axios.isAxiosError(error)) {
         console.error("Error creating task", error);
       } else {
-        throw new Error("Some server error occurred");
+        throw new Error("Some server error occurred.");
       }
     }
   };
